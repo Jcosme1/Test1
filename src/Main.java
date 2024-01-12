c507b13761f9635a16fdbc96686b171cc8c1f0b3
Julian Cosmeu <Jcosme1@ggc.edu>
2024-01-12 11:33:38 -0500

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    //pracitce with a while loop/ a do while loop for wednesday!
+    public static void main(String[] args) {
+
+        System.out.println("Hello professor!");
+    }
+
+
+}
