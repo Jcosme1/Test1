2963e6b637a290b75022e9872aeb22f64ea333bf
Julian Cosmeu <Jcosme1@ggc.edu>
2024-01-12 12:31:33 -0500

    var practice

--- src/VariablePractice.java
+++ src/VariablePractice.java
@@ -2,12 +2,13 @@ public class VariablePractice {
 
         public static void main(String[] args) {
 //fjfghj
+            int temp = 25;
+            String firstName = "Julian ";
+            String lastName = "Cosme";
             System.out.println(temp);
             System.out.println(firstName + lastName);
 
-            int temp = 25;
-            String firstName = "Julian";
-            String lastName = "Cosme";
+
         }
 
 
