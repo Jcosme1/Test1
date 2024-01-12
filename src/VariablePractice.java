5d1daccaca845d3599185c8202209ff0644a7996
Julian Cosmeu <Jcosme1@ggc.edu>
2024-01-12 11:59:13 -0500

    testing

--- src/VariablePractice.java
+++ src/VariablePractice.java
@@ -2,7 +2,7 @@ public class VariablePractice {
 
 
         public static void main(String[] args) {
-
+//s
             System.out.println(temp);
             System.out.println(firstName + lastName);
 
